0b9a6a4d87904b86dd0961035ee8ffc75775a4aa
giginet <giginet.net@gmail.com>
2022-08-08 20:25:51 +0900

    Revert "Remove unnecessary processes"
    This reverts commit 6c02d91bff08546601f801f60f23b03ec54f5316.

--- Sources/ScipioKit/Scipio/ProjectGenerator.swift
+++ Sources/ScipioKit/Scipio/ProjectGenerator.swift
@@ -29,7 +29,84 @@ struct ProjectGenerator {
             fileSystem: fileSystem,
             observabilityScope: observabilitySystem.topScope)
 
+        let distributionXCConfig = RelativePath("Distribution.xcconfig")
+        try fileSystem.writeFileContents(AbsolutePath(outputDirectory, distributionXCConfig),
+                                         string: distributionXCConfigContents)
+
+        let group = createOrGetConfigsGroup(project: project)
+        let reference = group.addFileReference (
+            path: distributionXCConfig.pathString,
+            name: distributionXCConfig.basename
+        )
+
+        for target in project.frameworkTargets {
+            target.buildSettings.xcconfigFileRef = reference
+        }
+
+        for target in project.frameworkTargets {
+            let name = "\(target.name.spm_mangledToC99ExtendedIdentifier())_Info.plist"
+            let path = projectPath.appending(RelativePath(name))
+            try fileSystem.writeFileContents(path) { stream in
+                stream.write(infoPlist)
+            }
+        }
+
+        try fileSystem.writeFileContents(projectPath.appending(component: "project.pbxproj")) { stream in
+            // Serialize the project model we created to a plist, and return
+            // its string description.
+            if let plist = try? project.generatePlist() {
+                let str = "// !$*UTF8*$!\n" + plist.description
+                stream.write(str)
+            }
+        }
+
         return .init(project: project, projectPath: projectPath)
+    }
+
+    private var distributionXCConfigContents: String {
+        """
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+        """
+    }
+
+    private var infoPlist: String {
+        """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <plist version="1.0">
+        <dict>
+        <key>CFBundleDevelopmentRegion</key>
+        <string>en</string>
+        <key>CFBundleExecutable</key>
+        <string>$(EXECUTABLE_NAME)</string>
+        <key>CFBundleIdentifier</key>
+        <string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
+        <key>CFBundleInfoDictionaryVersion</key>
+        <string>6.0</string>
+        <key>CFBundleName</key>
+        <string>$(PRODUCT_NAME)</string>
+        <key>CFBundlePackageType</key>
+        <string>FMWK</string>
+        <key>CFBundleShortVersionString</key>
+        <string>1.0</string>
+        <key>CFBundleSignature</key>
+        <string>????</string>
+        <key>CFBundleVersion</key>
+        <string>$(CURRENT_PROJECT_VERSION)</string>
+        <key>NSPrincipalClass</key>
+        <string></string>
+        </dict>
+        </plist>
+        """
+    }
+
+    private func createOrGetConfigsGroup(project: Xcode.Project) -> Xcode.Group {
+        let name = "Configs"
+
+        if let group = project.mainGroup.subitems.lazy.compactMap({ $0 as? Xcode.Group }).first(where: { $0.name == name }) {
+            return group
+        }
+
+        return project.mainGroup.addGroup(path: "", name: name)
     }
 }
 
---
+++ Tests/ScipioKitTests/ScipioTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import ScipioKit
+
+final class ScipioTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(Scipio().text, "Hello, World!")
+    }
+}
