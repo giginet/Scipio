196ab14e17320d29339e8df0a8a2eff41cf97fea
giginet <giginet.net@gmail.com>
2023-01-11 19:47:38 +0900

    Support settings flags on manifests

---
+++ Sources/ScipioKit/ProjectGenerator/BuildFlagsGenerator.swift
@@ -0,0 +1,130 @@
+import Foundation
+import PackageModel
+import PackageGraph
+
+struct BuildFlagsGenerator {
+    private let package: Package
+    private let buildConfiguration: BuildConfiguration
+    private let platforms: Set<SDK>
+
+    enum Error: LocalizedError {
+        case unsupportedSettings(TargetBuildSettingDescription.Setting)
+
+        var errorDescription: String? {
+            switch self {
+            case .unsupportedSettings(let setting):
+                return "Unsupported settings \(setting.kind) for \(setting.tool)"
+            }
+        }
+    }
+
+    init(package: Package, buildConfiguration: BuildConfiguration, platforms: Set<SDK>) {
+        self.package = package
+        self.buildConfiguration = buildConfiguration
+        self.platforms = platforms
+    }
+
+    func generate(for target: ResolvedTarget) throws -> [String: XCConfigValue] {
+        guard let targetDescription = package.manifest.targets.first(where: { $0.name == target.name }) else {
+            return [:]
+        }
+        return try targetDescription.settings
+            .filter { setting in
+                if let condition = setting.condition {
+                    return self.satisfyConditions(condition)
+                } else {
+                    return true
+                }
+            }
+            .reduce(into: [:]) { settings, target in
+                settings.merge(try xcodeprojSettings(for: target))
+            }
+    }
+
+    private func xcodeprojSettings(for setting: TargetBuildSettingDescription.Setting) throws -> [String: XCConfigValue] {
+        switch setting.tool {
+        case .c:
+            return try cSettings(for: setting)
+        case .cxx:
+            return try cxxSettings(for: setting)
+        case .swift:
+            return try swiftSettings(for: setting)
+        case .linker:
+            return try linkerSettings(for: setting)
+        }
+    }
+
+
+    private func satisfyConditions(_ condition: PackageConditionDescription) -> Bool {
+        Set(condition.platformNames).isSuperset(of: platforms.map(\.rawValue)) && condition.config == buildConfiguration.settingsValue
+    }
+
+    private func resolvePath(for value: String) -> String {
+        let targetRoot = package.packageDirectory
+        return targetRoot.appendingPathComponent(value).path
+    }
+
+    private func cSettings(for setting: TargetBuildSettingDescription.Setting) throws -> [String: XCConfigValue] {
+        precondition(setting.tool == .c, "invalid tool")
+        switch setting.kind {
+        case .headerSearchPath(let value):
+            return ["HEADER_SEARCH_PATHS": .string(resolvePath(for: value))]
+        case .define(let value):
+            return ["GCC_PREPROCESSOR_DEFINITIONS": .string(value)]
+        case .linkedFramework:
+            throw Error.unsupportedSettings(setting)
+        case .linkedLibrary:
+            throw Error.unsupportedSettings(setting)
+        case .unsafeFlags(let value):
+            return ["OTHER_CFLAGS": .list(value)]
+        }
+    }
+
+    private func cxxSettings(for setting: TargetBuildSettingDescription.Setting) throws -> [String: XCConfigValue] {
+        precondition(setting.tool == .cxx, "invalid tool")
+        switch setting.kind {
+        case .headerSearchPath(let value):
+            return ["HEADER_SEARCH_PATHS": .string(resolvePath(for: value))]
+        case .define(let value):
+            return ["GCC_PREPROCESSOR_DEFINITIONS": .string(value)]
+        case .linkedFramework:
+            throw Error.unsupportedSettings(setting)
+        case .linkedLibrary:
+            throw Error.unsupportedSettings(setting)
+        case .unsafeFlags(let value):
+            return ["OTHER_CPLUSPLUSFLAGS": .list(value)]
+        }
+    }
+
+    private func swiftSettings(for setting: TargetBuildSettingDescription.Setting) throws -> [String: XCConfigValue] {
+        precondition(setting.tool == .swift, "invalid tool")
+        switch setting.kind {
+        case .headerSearchPath:
+            throw Error.unsupportedSettings(setting)
+        case .define(let value):
+            return ["SWIFT_ACTIVE_COMPILATION_CONDITIONS": .string(value)]
+        case .linkedFramework:
+            throw Error.unsupportedSettings(setting)
+        case .linkedLibrary:
+            throw Error.unsupportedSettings(setting)
+        case .unsafeFlags(let value):
+            return ["OTHER_SWIFT_FLAGS": .list(value)]
+        }
+    }
+
+    private func linkerSettings(for setting: TargetBuildSettingDescription.Setting) throws -> [String: XCConfigValue] {
+        precondition(setting.tool == .linker, "invalid tool")
+        switch setting.kind {
+        case .headerSearchPath:
+            throw Error.unsupportedSettings(setting)
+        case .define:
+            throw Error.unsupportedSettings(setting)
+        case .linkedFramework(let value):
+            return ["LINK_LIBRARIES": .string(value)]
+        case .linkedLibrary(let value):
+            return ["LINK_FRAMEWORKS": .string(value)]
+        case .unsafeFlags(let value):
+            return ["OTHER_LDFLAGS": .list(value)]
+        }
+    }
+}
