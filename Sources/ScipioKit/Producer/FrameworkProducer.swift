8704b8d66f78cccd81c46c6d02897c834c7b60c6
giginet <giginet.net@gmail.com>
2023-01-23 16:31:28 +0900

    Abstract Compiler

--- Sources/ScipioKit/Producer/FrameworkProducer.swift
+++ Sources/ScipioKit/Producer/FrameworkProducer.swift
@@ -3,7 +3,7 @@ import PackageGraph
 import PackageModel
 import OrderedCollections
 
-struct XcodebuildFrameworkProducer {
+struct FrameworkProducer {
     private let mode: Runner.Mode
     private let rootPackage: Package
     private let buildOptions: BuildOptions
