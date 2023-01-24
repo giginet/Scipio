743c9a39b9465f41a4758b4680f8265ed831ca93
giginet <giginet.net@gmail.com>
2023-01-25 01:01:56 +0900

    Implement integration tests

--- Tests/ScipioKitTests/IntegrationTests.swift
+++ Tests/ScipioKitTests/IntegrationTests.swift
@@ -1,6 +1,7 @@
 import Foundation
 import XCTest
-import ScipioKit
+@testable import ScipioKit
+import Logging
 
 private let fixturePath = URL(fileURLWithPath: #file)
     .deletingLastPathComponent()
@@ -20,6 +21,7 @@ final class IntegrationTests: XCTestCase {
 
     override func setUp() async throws {
         try XCTSkipUnless(Self.integrationTestsEnabled)
+
         try await super.setUp()
     }
 
@@ -30,10 +32,10 @@ final class IntegrationTests: XCTestCase {
                 buildConfiguration: .release,
                 isSimulatorSupported: false,
                 isDebugSymbolsEmbedded: false,
-                frameworkType: .dynamic,
+                frameworkType: .static,
                 cacheMode: .disabled,
-                overwrite: false,
-                verbose: true)
+                overwrite: true,
+                verbose: false)
         )
         let outputDir = fileManager.temporaryDirectory
             .appendingPathComponent("Scipio")
@@ -42,5 +44,66 @@ final class IntegrationTests: XCTestCase {
             packageDirectory: integrationTestPackagePath,
             frameworkOutputDir: .custom(outputDir)
         )
+        addTeardownBlock {
+            try self.fileManager.removeItem(atPath: outputDir.path)
+        }
+
+        let expectedFrameworks = [
+            "Atomics",
+            "Collections",
+            "DequeModule",
+            "Logging",
+            "OrderedCollections",
+            "_AtomicsShims",
+        ]
+
+        let destination = "ios-arm64"
+        let outputDirContents = try fileManager.contentsOfDirectory(atPath: outputDir.path)
+
+        for frameworkName in expectedFrameworks {
+            let xcFrameworkName = "\(frameworkName).xcframework"
+            XCTAssertTrue(
+                outputDirContents.contains(xcFrameworkName),
+                "\(xcFrameworkName) should be built"
+            )
+
+            let frameworkRoot = outputDir
+                .appendingPathComponent(xcFrameworkName)
+                .appendingPathComponent(destination)
+                .appendingPathComponent("\(frameworkName).framework")
+
+            let isPrivateFramework = frameworkName.hasPrefix("_")
+
+            if !isPrivateFramework {
+                XCTAssertTrue(
+                    fileManager.fileExists(atPath: frameworkRoot.appendingPathComponent("Headers/\(frameworkName)-Swift.h").path),
+                    "\(xcFrameworkName) should contain a bridging header"
+                )
+
+                XCTAssertTrue(
+                    fileManager.fileExists(atPath: frameworkRoot.appendingPathComponent("Modules/\(frameworkName).swiftmodule").path),
+                    "\(xcFrameworkName) should contain swiftmodules"
+                )
+            }
+
+            XCTAssertTrue(
+                fileManager.fileExists(atPath: frameworkRoot.appendingPathComponent("Modules/module.modulemap").path),
+                "\(xcFrameworkName) should contain a module map"
+            )
+
+            let binaryPath = frameworkRoot.appendingPathComponent(frameworkName)
+            XCTAssertTrue(
+                fileManager.fileExists(atPath: binaryPath.path),
+                "\(xcFrameworkName) should contain a binary"
+            )
+
+            let executor = ProcessExecutor()
+            let result = try await executor.execute("/usr/bin/file", binaryPath.path)
+            let output = try XCTUnwrap(try result.unwrapOutput())
+            XCTAssertTrue(
+                output.contains("current ar archive"),
+                "\(xcFrameworkName) must be a static framework"
+            )
+        }
     }
 }
