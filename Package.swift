e9465e047e86e70f2d2b09be29c1eb2ce3cd2741
giginet <giginet.net@gmail.com>
2023-02-01 20:18:08 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git",
                  .upToNextMinor(from: "1.4.2")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", 
-                 .upToNextMinor(from: "1.0.0")),
+                 .upToNextMinor(from: "1.1.0")),
         .package(url: "https://github.com/onevcat/Rainbow",
                  .upToNextMinor(from: "4.0.1")),
         .package(url: "https://github.com/tuist/XcodeProj.git",
