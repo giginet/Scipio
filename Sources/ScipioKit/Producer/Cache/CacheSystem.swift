8e6b8833f5db0e8e5f4dedfd0a043709aa0d679c
giginet <giginet.net@gmail.com>
2023-02-22 14:17:30 +0900

    Publish cache key

--- Sources/ScipioKit/Producer/Cache/CacheSystem.swift
+++ Sources/ScipioKit/Producer/Cache/CacheSystem.swift
@@ -218,7 +218,7 @@ struct CacheSystem {
 }
 
 extension CacheKey {
-    func calculateChecksum() throws -> String {
+    public func calculateChecksum() throws -> String {
         let data = try jsonEncoder.encode(self)
         return SHA256().hash(ByteString(data)).hexadecimalRepresentation
     }
