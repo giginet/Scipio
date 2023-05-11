ef82a05df9fca2b9106bb6d2ce7899fac08c13cf
giginet <giginet.net@gmail.com>
2023-05-11 23:59:31 +0900

    Refactor plugin

--- Plugins/RevisionFetcher/plugin.swift
+++ Plugins/RevisionFetcher/plugin.swift
@@ -4,28 +4,12 @@ import PackagePlugin
 @main
 struct GenerateScipioVersion: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        let standardOutput = Pipe()
-        let process = Process()
-        process.executableURL = URL(fileURLWithPath: "/usr/bin/git")
-        process.arguments = [
-            "-C",
-            context.package.directory.string,
-            "rev-parse",
-            "HEAD",
-        ]
-        process.standardOutput = standardOutput
-
-        try process.run()
-        process.waitUntilExit()
-        let outputData = try standardOutput.fileHandleForReading.readToEnd()
-        guard let outputData else { return [] }
-        let revision = String(data: outputData, encoding: .utf8)?
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-
         let zshPath = Path("/bin/zsh") // execute dummy command
         let generatedSourceDir = context.pluginWorkDirectory
         let generatedSourcePath = generatedSourceDir
             .appending(subpath: "ScipioVersion.generated.swift")
+
+        let revision = try? fetchRepositoryVersion(context: context)
 
         let fileContents: String
         if let revision {
@@ -49,5 +33,26 @@ struct GenerateScipioVersion: BuildToolPlugin {
                 outputFilesDirectory: generatedSourceDir
             ),
         ]
+    }
+
+    private func fetchRepositoryVersion(context: PluginContext) throws -> String? {
+        let standardOutput = Pipe()
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/git")
+        process.arguments = [
+            "-C",
+            context.package.directory.string,
+            "rev-parse",
+            "HEAD",
+        ]
+        process.standardOutput = standardOutput
+
+        try process.run()
+        process.waitUntilExit()
+        let outputData = try standardOutput.fileHandleForReading.readToEnd()
+        guard let outputData else { return nil }
+        let revision = String(data: outputData, encoding: .utf8)?
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        return revision
     }
 }
