ca15f2630fbd6bf66ed355139c623dd46de0b2b7
giginet <giginet.net@gmail.com>
2023-05-11 20:45:06 +0900

    Bump up version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Scipio",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     products: [
         .executable(name: "scipio",
