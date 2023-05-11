74d5b952ec9144bdcf21fa73aa1237a874283d2d
giginet <giginet.net@gmail.com>
2023-05-11 23:44:21 +0900

    Use git to fetch revision

--- Plugins/RevisionFetcher/plugin.swift
+++ Plugins/RevisionFetcher/plugin.swift
@@ -4,28 +4,37 @@ import PackagePlugin
 @main
 struct GenerateScipioVersion: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let standardOutput = Pipe()
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/git")
+        process.arguments = [
+            "-C",
+            context.package.directory.string,
+            "rev-parse",
+            "HEAD",
+        ]
+        process.standardOutput = standardOutput
+
+        try process.run()
+        process.waitUntilExit()
+        let outputData = try standardOutput.fileHandleForReading.readToEnd()
+        guard let outputData else { return [] }
+        let revision = String(data: outputData, encoding: .utf8)?
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+
         let zshPath = Path("/bin/zsh") // execute dummy command
         let generatedSourceDir = context.pluginWorkDirectory
         let generatedSourcePath = generatedSourceDir
             .appending(subpath: "ScipioVersion.generated.swift")
 
-        let versionName: String?
-        print(context.package.dependencies.map(\.package.displayName))
-        if let scipioPackage = context.package.dependencies.first(where: { $0.package.displayName.lowercased() == "scipio" })?.package {
-            print(scipioPackage.origin)
-            versionName = fetchVersionName(of: scipioPackage.origin)
-        } else {
-            versionName = nil
-        }
-
         let fileContents: String
-        if let versionName {
-            fileContents = #"let currentScipioVersion: String? = "\#(versionName)""#
+        if let revision {
+            fileContents = #"let currentScipioVersion: String? = "\#(revision)""#
         } else {
             fileContents = #"let currentScipioVersion: String? = nil"#
         }
 
-        print("Current scipio version is \(versionName ?? "unknown")")
+        print("Current scipio version is \(revision ?? "unknown")")
 
         FileManager.default.createFile(
             atPath: generatedSourcePath.string,
@@ -40,20 +49,5 @@ struct GenerateScipioVersion: BuildToolPlugin {
                 outputFilesDirectory: generatedSourceDir
             ),
         ]
-    }
-
-    private func fetchVersionName(of packageOrigin: PackageOrigin) -> String? {
-        switch packageOrigin {
-        case .root:
-            return "root"
-        case .local(_):
-            return "local"
-        case .repository(_, _, let scmRevision):
-            return scmRevision
-        case .registry(_, let displayVersion):
-            return displayVersion
-        @unknown default:
-            return nil
-        }
     }
 }
