3683badc60e30998a1407a85d52581a64769d2d5
omochimetaru <omochi.metaru@gmail.com>
2023-08-02 13:59:06 +0900

    remove macOS from package

--- Tests/ScipioKitTests/Resources/Fixtures/IntegrationTestPackage/Package.swift
+++ Tests/ScipioKitTests/Resources/Fixtures/IntegrationTestPackage/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "IntegrationTestPackage",
-    platforms: [.iOS(.v14), .macOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "IntegrationTestPackage",
