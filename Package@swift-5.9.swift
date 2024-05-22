4e79f30f3edb78cf4a35b5fa34d34f9fd2f0c6fa
giginet <giginet.net@gmail.com>
2024-05-22 13:43:35 +0900

    Update storage-protocol

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -30,7 +30,7 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Rainbow",
                  .upToNextMinor(from: "4.0.1")),
         .package(url: "https://github.com/giginet/scipio-cache-storage.git",
-                 revision: "ec3467983ae14356ffcba0ecf4e5c76c3dd80532"),
+                 revision: "a7b01bbf02dea506af7d8f7a13f6e985044d5d57"),
     ],
     targets: [
         .executableTarget(name: "scipio",
