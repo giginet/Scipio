f6cbef149f247b1a09d9966ead372625b298ba59
ikesyo <suicaicoca@gmail.com>
2024-11-11 15:43:10 +0900

    debugging

--- Sources/ScipioKit/Producer/Cache/CacheSystem.swift
+++ Sources/ScipioKit/Producer/Cache/CacheSystem.swift
@@ -245,7 +245,10 @@ struct CacheSystem: Sendable {
             buildOptions: buildOptions,
             clangVersion: clangVersion,
             xcodeVersion: xcodeVersion,
-            scipioVersion: "578ce98d236e79dad3e473cb11153e867be07174" // TODO: revert
+            // Making the cache key compatible with 0.24.0 temporarily for easier debugging.
+            //
+            // TODO: revert this before merging
+            scipioVersion: "578ce98d236e79dad3e473cb11153e867be07174"
         )
     }
 
