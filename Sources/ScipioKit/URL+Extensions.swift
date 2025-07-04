64f695f59596958a073d8331f9a8070567b880a5
Ryu0118 <ryunosuke.shibuya.3d@gmail.com>
2025-07-04 18:47:03 +0900

    Add ref comment

--- Sources/ScipioKit/URL+Extensions.swift
+++ Sources/ScipioKit/URL+Extensions.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 extension URL {
+    // ref: https://github.com/swiftlang/swift-tools-support-core/blob/f9b401016b70c6b8409e5c97e74d97513d1a8d02/Sources/TSCBasic/Path.swift#L598-L614
     var dirname: String {
         let path = self.standardizedFileURL.path
         guard let idx = path.lastIndex(of: "/") else {
@@ -15,6 +16,7 @@ extension URL {
         return String(path.prefix(upTo: idx))
     }
 
+    // ref: https://github.com/swiftlang/swift-tools-support-core/blob/f9b401016b70c6b8409e5c97e74d97513d1a8d02/Sources/TSCBasic/Path.swift#L661-L663
     var parentDirectory: URL {
         dirname.isEmpty ? self : deletingLastPathComponent()
     }
